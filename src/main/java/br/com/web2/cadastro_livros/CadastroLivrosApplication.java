b0c3268e2bc60383a0342c34f2ff8397ae6cdffa
eldersb <122701368+eldersb@users.noreply.github.com>
2024-09-03 14:29:29 -0300

    Commit inicial

---
+++ src/main/java/br/com/web2/cadastro_livros/CadastroLivrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.web2.cadastro_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/web2/cadastro_livros/CadastroLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.web2.cadastro_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
